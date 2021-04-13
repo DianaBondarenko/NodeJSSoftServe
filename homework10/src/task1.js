8d00100bf133893cbca2380294b3d11833d7298f
Diana <57017814+DianaBondarenko@users.noreply.github.com>
2021-04-13 20:53:03 +0300

    Update task1.js

--- homework10/src/task1.js
+++ homework10/src/task1.js
@@ -1,8 +1,14 @@
+const { validation1 } = require('./validation.js')
 exports.chessBoard = function (height, width, symbol) {
-    let result = ``;
-    let getLine = (line) => line.repeat(width / 2) + '\n';
-    for (let i = 0; i < height; i++) {
-        result += (i % 2) ? getLine((' ' + symbol[0])) : getLine((symbol[0] + ' '));
-    }
-    return result;
+    const validation = validation1(height,width,symbol, arguments.length);
+    console.log(validation);
+    if (validation === true) {
+        let result = ``;
+        let getLine = (line) => line.repeat(width / 2) + '\n';
+        for (let i = 0; i < height; i++) {
+            result += (i % 2) ? getLine((' ' + symbol[0])) : getLine((symbol[0] + ' '));
+        }
+        return result;
+    } else return validation;
+
 }
