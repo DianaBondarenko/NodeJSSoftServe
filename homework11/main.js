ba6aa66e2245cdc5a85437efb442d41a0d159799
Diana <57017814+DianaBondarenko@users.noreply.github.com>
2021-04-16 00:40:46 +0300

    Add files via upload

---
+++ homework11/main.js
@@ -0,0 +1,39 @@
+import {checkName, checkLogin, checkPassw, checkEmail, checkDate} from './validation.js'
+
+const regForm = document.getElementById('regForm');
+const authForm = document.getElementById('authForm');
+
+regForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputs = Array.from(document.querySelectorAll('#regForm input')).map(el => el.value);
+    let [name, surname, login, password, email, dob] = inputs;
+    if (checkName(name) && checkName(surname) && checkLogin(login) && checkPassw(password) && checkEmail(email) && checkDate(dob)) {
+        let xhr = new XMLHttpRequest();
+        dob = dob.replaceAll('-', '/');
+        let url = `http://localhost:8080/reg/?name=${name}&surname=${surname}&login=${login}&password=${password}&email=${email}&dob=${dob}`;
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            alert(xhr.responseText);
+        }
+        xhr.send();
+    } else alert('Incorrect data!');
+})
+authForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputs = Array.from(document.querySelectorAll('#authForm input')).map(el => el.value);
+    let [login, password] = inputs;
+    if (checkLogin(login) && checkPassw(password)) {
+        let xhr = new XMLHttpRequest();
+        let url = `http://localhost:8081/auth/?login=${login}&password=${password}`;
+        console.log(url);
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            alert(xhr.responseText);
+        }
+        xhr.send();
+    } else alert('Incorrect data!');
+
+    // if (checkStr50(inputs[0].value) && checkPassw(inputs[1].value)) {
+    //     authForm.submit();
+    // } else alert('Incorrect data!');
+})
